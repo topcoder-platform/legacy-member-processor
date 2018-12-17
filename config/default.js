616a7f0488c10f54e44c95b8e00e1dd7f703152f
urwithat <er.atif@gmail.com>
2018-12-17 17:18:01 +0530

    Changed env variable names

--- config/default.js
+++ config/default.js
@@ -6,11 +6,11 @@ module.exports = {
   DISABLE_LOGGING: process.env.DISABLE_LOGGING || false, // If true, logging will be disabled
   LOG_LEVEL: process.env.LOG_LEVEL || 'debug',
 
-  KAFKA_URL: process.env.KAFKA_URL_DEV,
+  KAFKA_URL: process.env.KAFKA_URL,
   // below are used for secure Kafka connection, they are optional
   // for the local Kafka, they are not needed
-  KAFKA_CLIENT_CERT: process.env.KAFKA_CLIENT_CERT_DEV,
-  KAFKA_CLIENT_CERT_KEY: process.env.KAFKA_CLIENT_CERT_KEY_DEV,
+  KAFKA_CLIENT_CERT: process.env.KAFKA_CLIENT_CERT,
+  KAFKA_CLIENT_CERT_KEY: process.env.KAFKA_CLIENT_CERT_KEY,
 
   CREATE_PROFILE_TOPIC: process.env.CREATE_PROFILE_TOPIC || 'member.action.profile.create',
   UPDATE_PROFILE_TOPIC: process.env.UPDATE_PROFILE_TOPIC || 'member.action.profile.update',
