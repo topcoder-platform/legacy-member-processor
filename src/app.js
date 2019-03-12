1180831b67cb800dbb835287c61dccbdc1b190d3
Thomas Kranitsas <kranitsasthomas@gmail.com>
2019-03-12 22:45:24 +0200

    Remove logging sensitive info

--- src/app.js
+++ src/app.js
@@ -15,15 +15,6 @@ const options = getKafkaOptions()
 console.log('DISABLE_LOGGING :: ' + config.DISABLE_LOGGING)
 console.log('DISABLE_LOGGING :: ' + typeof (config.DISABLE_LOGGING))
 
-logger.info('Starting the application........')
-logger.info('KAFKA_URL - ' + config.KAFKA_URL)
-logger.info('KAFKA_CLIENT_CERT - ' + config.KAFKA_CLIENT_CERT)
-logger.info('KAFKA_CLIENT_CERT_KEY - ' + config.KAFKA_CLIENT_CERT_KEY)
-
-logger.info('IFX_SERVER - ' + config.IFX_SERVER)
-logger.info('IFX_DATABASE - ' + config.IFX_DATABASE)
-logger.info('INFORMIX_HOST - ' + config.INFORMIX_HOST)
-logger.info(options)
 const consumer = new Kafka.GroupConsumer(options)
 
 // data handler
